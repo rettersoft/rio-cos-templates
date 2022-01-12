0482e84ad0ad4b8c284e19de0e05e054833235e0
Halil Savaşcı <halilsavasci32@gmail.com>
2022-01-12 15:24:11 +0300

    initial commit

---
+++ Default/classes/Test/index.ts
@@ -0,0 +1,23 @@
+import RDK, { Data, InitResponse, Response, StepResponse } from "@retter/rdk";
+
+const rdk = new RDK();
+
+export async function preAuthorizer(data: Data): Promise<Response> {
+    return { statusCode: 200 };
+}
+
+export async function init(data: Data): Promise<InitResponse> {
+    return { state: { public: { message: "Hello World" } } };
+}
+
+export async function getState(data: Data): Promise<Response> {
+    return { statusCode: 200, body: data.state };
+}
+
+export async function sayHello(data: Data): Promise<StepResponse> {
+    data.response = {
+        statusCode: 200,
+        body: { message: data.state.public.message },
+    };
+    return data;
+}
